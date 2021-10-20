ee41188d7bb2ec160f6930fea52af20e2b33329d
wild-hawk-SAS <sreekanth_official@outlook.com>
2021-10-20 18:49:01 +0530

    committed

---
+++ rest/restapp/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from.serializers import TaskSerializers,UserSerializers
+from.models import Task
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated,AllowAny
+from django.contrib.auth import get_user_model
+from rest_framework.generics import CreateAPIView
+from rest_framework import generics
+
+# Create your views here.
+
+class TaskViewset(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    queryset = Task.objects.all().order_by('-date_created')
+    serializer_class = TaskSerializers
+
+class DueTaskViewset(viewsets.ModelViewSet):
+    queryset = Task.objects.all().order_by('-date_created').filter(completed=False)
+    serializer_class = TaskSerializers
+
+class CreateuserView(CreateAPIView):
+    model = get_user_model()
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializers
+
+class CompletedTaskViewset(viewsets.ModelViewSet):
+    queryset = Task.objects.all().order_by('-date_created').filter(completed=True)
+    serializer_class = TaskSerializers
+
+class update(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task
+    serializer_class = TaskSerializers
